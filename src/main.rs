0d903305126211a09e7f5a28747390543e75cb41
Bakuwan <f4dhliraihan@gmail.com>
2025-03-21 18:49:12 +0700

    (5) Multithreaded server using Threadpool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use hello::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
@@ -5,13 +6,20 @@ use std::{
     thread, 
     time::Duration,
    };
-fn main() {
+   fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
-    let stream = stream.unwrap();
-    handle_connection(stream); 
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
+
+
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
